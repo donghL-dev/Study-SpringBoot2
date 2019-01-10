a8125e64b9c7a3d8def9a38876b224fc3aa6fe3c
dongh9508 <odh621@naver.com>
2019-01-10 17:18:54 +0900

    create day_1 directory

---
+++ day_1/README.md
@@ -0,0 +1,5 @@
+Study-Day-1
+===
+* application.yml 생성 및 test property 값 추가 
+* @Value 어노테이션 테스트
+* @ConfigurationProperties 어노테이션 테스트
--- day_1/community/src/main/java/com/community/CommunityApplication.java
+++ day_1/community/src/main/java/com/community/CommunityApplication.java
@@ -3,9 +3,7 @@ package com.community;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class CommunityApplication {
 
